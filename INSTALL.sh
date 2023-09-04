52ab8f7688db8a0b93857d52a4b1ddef9972116a
fkia <fkia@protonmail.com>
2023-09-04 22:34:28 +0330

    update

--- INSTALL.sh
+++ INSTALL.sh
@@ -3,16 +3,11 @@ set -euo pipefail
 
 [[ $UID == "0" ]] || { echo "You are not root." >&2; exit 1; }
 
-install -m 644 ./bash_colors /etc/profile.d/bash_colors.sh && \
-    echo -e "Installed: /etc/profile.d/bash_colors.sh"
+echo -e "\nInstalling...\n"
 
-install -m 644 ./loading /etc/profile.d/loading.sh && \
-    echo -e "Installed: /etc/profile.d/loading.sh"
-
-install -m 644 ./network /etc/profile.d/network.sh && \
-    echo -e "Installed: /etc/profile.d/network.sh"
-
-install -m 644 ./utils /etc/profile.d/utils.sh && \
-    echo -e "Installed: /etc/profile.d/utils.sh"
+install -v -m 644 ./bash_colors /etc/profile.d/bash_colors.sh
+install -v -m 644 ./loading /etc/profile.d/loading.sh
+install -v -m 644 ./network /etc/profile.d/network.sh
+install -v -m 644 ./utils /etc/profile.d/utils.sh
 
 echo -e "\nDone. Please login again.\n"
