2e0a49446797f6e48c206c6bd5ec78ffdddf2648
fkia <fkia@protonmail.com>
2025-06-04 21:03:05 +0330

    fixed directory names

--- INSTALL.sh
+++ INSTALL.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 [[ $UID == "0" ]] || { echo "You are not root." >&2; exit 1; }
+DIR=$(dirname "$0")
 
 echo -e "\nInstalling...\n"
 
-install -v -m 644 ./bash_colors /etc/profile.d/bash_colors.sh
-install -v -m 644 ./loading /etc/profile.d/loading.sh
-install -v -m 644 ./network /etc/profile.d/network.sh
-install -v -m 644 ./utils /etc/profile.d/utils.sh
+install -v -m 644 "$DIR/bash_colors" /etc/profile.d/bash_colors.sh
+install -v -m 644 "$DIR/loading" /etc/profile.d/loading.sh
+install -v -m 644 "$DIR/network" /etc/profile.d/network.sh
+install -v -m 644 "$DIR/utils" /etc/profile.d/utils.sh
 
 [[ $1 == "-q" ]] || echo -e "\nDone. Please login again.\n"
